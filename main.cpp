3af99053803c8151e80ca374569b3440c617fe13
skim <magiclogy@gmail.com>
2017-08-23 11:16:27 +0800

    i18n prepare

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include "minecraftwrapper.hpp"
 #include <QApplication>
-#include <QDebug>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 #include "mcconfig.hpp"
 #include "modswitch.hpp"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(),
+            QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
+
+    QTranslator mwTranslator;
+    mwTranslator.load("mw_" + QLocale::system().name());
+    app.installTranslator(&mwTranslator);
+
+
     MinecraftWrapper w;
     w.show();
 
-    return a.exec();
+    return app.exec();
 }
